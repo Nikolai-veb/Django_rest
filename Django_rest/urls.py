d72298fc483629ec64649c8294ce3c1393372d8f
Nikolay <nik.vagin1995@mail.ru>
2020-11-09 23:02:44 +0400

    Create auto documenteitions with help library drf-yasg

--- Django_rest/urls.py
+++ Django_rest/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import  settings
+from django.conf.urls.static import static
+from .yasg import urlpatterns as doc_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,8 @@ urlpatterns = [
     path("api/v1/", include("shop.urls")),
 
 ]
+
+urlpatterns += doc_url
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
